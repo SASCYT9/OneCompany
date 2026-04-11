cf32949cd8403016d7469aa158c5cc92071dfc7f
SASCYT9 <sashatsompel@gmail.com>
2026-04-11 22:34:52 +0300

    fix: prisma import for NextJS build

--- src/app/api/shopify/auth/callback/route.ts
+++ src/app/api/shopify/auth/callback/route.ts
@@ -1,6 +1,6 @@
 import { NextRequest, NextResponse } from 'next/server';
 import crypto from 'crypto';
-import prisma from '@/lib/prisma';
+import { prisma } from '@/lib/prisma';
 
 export async function GET(req: NextRequest) {
   const url = new URL(req.url);
